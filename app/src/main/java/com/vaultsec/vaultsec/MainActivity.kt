0e7958e337573b4bcd4bba4c42e02c57d12690c1
Eitvidas Puras <peitvis@gmail.com>
2020-04-15 23:11:35 +0300

    Login screen and icons

--- app/src/main/java/com/vaultsec/vaultsec/MainActivity.kt
+++ app/src/main/java/com/vaultsec/vaultsec/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.vaultsec.vaultsec
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+//        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE,
+//            WindowManager.LayoutParams.FLAG_SECURE)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            window.attributes.layoutInDisplayCutoutMode =
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        }
     }
+
+
 }
