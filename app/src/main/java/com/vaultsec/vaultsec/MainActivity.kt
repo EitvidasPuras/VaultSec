c4b0327460c7919d442aff8c7002d79d70e53fca
Eitvidas Puras <peitvis@gmail.com>
2020-04-17 17:37:50 +0300

    Minor fixes to layouts. Login credentials validation

--- app/src/main/java/com/vaultsec/vaultsec/MainActivity.kt
+++ app/src/main/java/com/vaultsec/vaultsec/MainActivity.kt
@@ -3,13 +3,18 @@ package com.vaultsec.vaultsec
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Remove these comments to prevent taking screenshots
 //        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE,
 //            WindowManager.LayoutParams.FLAG_SECURE)
         super.onCreate(savedInstanceState)
@@ -19,6 +24,26 @@ class MainActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
         }
         openRegistrationActivity()
+        logUserIn()
+        clearInputErrors()
+    }
+
+    private fun clearInputErrors() {
+        textfield_login_email.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                textfield_login_email_layout.error = null
+            }
+        })
+
+        textfield_login_password.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                textfield_login_password_layout.error = null
+            }
+        })
     }
 
     private fun openRegistrationActivity() {
@@ -26,5 +51,28 @@ class MainActivity : AppCompatActivity() {
             val registrationIntent = Intent(this, RegistrationActivity::class.java)
             startActivity(registrationIntent)
         }
+    }
+
+    private fun logUserIn() {
+        button_login.setOnClickListener {
+            val emailInput: String = textfield_login_email.text.toString()
+            val passInput: String = textfield_login_password.text.toString()
+
+            if (loginCredentialsValidation(emailInput, passInput))
+                Toast.makeText(
+                    this, "email: $emailInput password: $passInput", Toast.LENGTH_SHORT
+                ).show()
+        }
+    }
+
+    private fun loginCredentialsValidation(emailInput: String, passInput: String): Boolean {
+        if (emailInput.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            textfield_login_email_layout.error = "Email is required"
+        }
+        if (passInput.isEmpty()) {
+            textfield_login_password_layout.error = "Password is required"
+        }
+        return textfield_login_email_layout.error.isNullOrEmpty()
+                && textfield_login_password_layout.error.isNullOrEmpty()
     }
 }
--- app/src/main/java/com/vaultsec/vaultsec/RegistrationActivity.kt
+++ app/src/main/java/com/vaultsec/vaultsec/RegistrationActivity.kt
@@ -3,13 +3,21 @@ package com.vaultsec.vaultsec
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Remove these comments to prevent taking screenshots
+//        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE,
+//            WindowManager.LayoutParams.FLAG_SECURE)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
@@ -17,12 +25,112 @@ class RegistrationActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
         }
         openLoginActivity()
+        registerUser()
+        clearInputErrors()
+    }
+
+    private fun clearInputErrors() {
+        textfield_registration_firstname.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                textfield_registration_firstname_layout.error = null
+            }
+        })
+        textfield_registration_lastname.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                textfield_registration_lastname_layout.error = null
+            }
+        })
+        textfield_registration_email.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                textfield_registration_email_layout.error = null
+            }
+        })
+        textfield_registration_password.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                textfield_registration_password_layout.error = null
+            }
+        })
+        textfield_registration_password_retype.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                textfield_registration_password_retype_layout.error = null
+            }
+        })
+    }
+
+    private fun registerUser() {
+        button_registration.setOnClickListener() {
+            val firstNameInput: String = textfield_registration_firstname.text.toString()
+            val lastNameInput: String = textfield_registration_lastname.text.toString()
+            val emailInput: String = textfield_registration_email.text.toString()
+            val passInput: String = textfield_registration_password.text.toString()
+            val passRetypeInput: String = textfield_registration_password_retype.text.toString()
+
+            if (registrationCredentialsValidation(
+                    firstNameInput, lastNameInput, emailInput, passInput, passRetypeInput
+                )
+            ) {
+                Toast.makeText(
+                    this,
+                    "length: ${firstNameInput.length}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun registrationCredentialsValidation(
+        firstNameInput: String, lastNameInput: String,
+        emailInput: String, passInput: String,
+        passRetypeInput: String
+    ): Boolean {
+        if (firstNameInput.length > 30
+            || lastNameInput.length > 30
+            || passInput.length > 30
+            || passRetypeInput.length > 30
+        ) {
+            return false
+        }
+
+        if (firstNameInput.isEmpty()) {
+            textfield_registration_firstname_layout.error = "First name is required"
+        } else if (!firstNameInput.chars().allMatch(Character::isLetter)) {
+            textfield_registration_firstname_layout.error = "First name must only contain letters"
+        }
+        if (lastNameInput.isEmpty()) {
+            textfield_registration_lastname_layout.error = "Last name is required"
+        } else if (!lastNameInput.chars().allMatch(Character::isLetter)) {
+            textfield_registration_lastname_layout.error = "First name must only contain letters"
+        }
+        if (emailInput.isEmpty()) {
+            textfield_login_email_layout.error = "Email is required"
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            textfield_registration_email_layout.error = "Email not valid"
+        }
+        if (passInput.isEmpty()) {
+            textfield_registration_password_layout.error = "Password is required"
+        }
+        if (passRetypeInput.isEmpty()) {
+            textfield_registration_password_retype_layout.error = "Field is required"
+        }
+        return true
     }
 
     private fun openLoginActivity() {
         textview_registration_login.setOnClickListener {
             val loginIntent = Intent(this, MainActivity::class.java)
+            loginIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
             startActivity(loginIntent)
+            finish()
         }
     }
 }
