00c86d26c4a30b317eedd27ef731c4a795e4b5c2
Eitvidas Puras <peitvis@gmail.com>
2021-04-21 22:39:15 +0300

    Implement fingerprint login on master password fragment

--- app/src/main/java/com/vaultsec/vaultsec/ui/masterpassword/MasterPasswordFragment.kt
+++ app/src/main/java/com/vaultsec/vaultsec/ui/masterpassword/MasterPasswordFragment.kt
@@ -1,7 +1,14 @@
 package com.vaultsec.vaultsec.ui.masterpassword
 
+import android.Manifest
+import android.app.KeyguardManager
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Build
 import android.os.Bundle
+import android.os.CancellationSignal
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
@@ -9,10 +16,13 @@ import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import androidx.activity.addCallback
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.snackbar.Snackbar
 import com.vaultsec.vaultsec.R
 import com.vaultsec.vaultsec.databinding.FragmentMasterPasswordBinding
 import com.vaultsec.vaultsec.ui.BottomNavigationActivity
@@ -28,6 +38,17 @@ class MasterPasswordFragment : Fragment(R.layout.fragment_master_password) {
 
     private val masterPasswordViewModel: MasterPasswordViewModel by viewModels()
 
+    private var cancellationSignal: CancellationSignal? = null
+
+    private val authenticationCallback: BiometricPrompt.AuthenticationCallback
+        get() = @RequiresApi(Build.VERSION_CODES.P)
+        object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                super.onAuthenticationSucceeded(result)
+                openBottomNavigationActivity()
+            }
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,6 +60,7 @@ class MasterPasswordFragment : Fragment(R.layout.fragment_master_password) {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -80,6 +102,50 @@ class MasterPasswordFragment : Fragment(R.layout.fragment_master_password) {
                 }
             }
         }
+
+        checkBiometricSupport()
+
+        val biometricPrompt = BiometricPrompt.Builder(requireContext())
+            .setTitle(getString(R.string.biometric_fingerprint_dialog_title))
+            .setSubtitle(getString(R.string.biometric_fingerprint_dialog_subtitle))
+            .setNegativeButton("Cancel", requireActivity().mainExecutor, { _, _ ->
+            }).build()
+
+        biometricPrompt.authenticate(
+            getCancellationSignal(),
+            requireActivity().mainExecutor,
+            authenticationCallback
+        )
+    }
+
+    private fun getCancellationSignal(): CancellationSignal {
+        cancellationSignal = CancellationSignal()
+        cancellationSignal?.setOnCancelListener {
+            Snackbar.make(
+                requireView(),
+                R.string.biometric_fingerprint_cancellation_signal,
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
+        return cancellationSignal as CancellationSignal
+    }
+
+    private fun checkBiometricSupport(): Boolean {
+        val keyguardManager =
+            requireActivity().getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+
+        if (!keyguardManager.isKeyguardSecure) {
+            return false
+        }
+
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.USE_BIOMETRIC)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            return false
+        }
+        return if (requireActivity().packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)) {
+            true
+        } else true
     }
 
     private fun openBottomNavigationActivity() {
